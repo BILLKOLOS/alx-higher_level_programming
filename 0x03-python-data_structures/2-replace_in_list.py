590e048818aea0906841a475449bd49a2ac95b23
BILLKOLOS <billooko2@gmail.com>
2023-03-12 22:41:26 -0700

    resolve errors

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
-def replace_in_list(my_list, idx, element):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    
-    # Replace the element at the specified index
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+#!/usr/bin/python3
 
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
