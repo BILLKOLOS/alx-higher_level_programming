3ce981b363e4dc80705c2ac401acb342aea73f94
BILLKOLOS <billooko2@gmail.com>
2023-03-27 11:35:03 -0700

    Write a function that prints x elements of a lists

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -8,4 +8,4 @@ print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+print("nb_print: {:d}".format(nb_print))`
