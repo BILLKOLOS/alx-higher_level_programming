475f92b14a4cc5350a5d8051666d3739a58d8819
billkolos <billooko2@gmail.com>
2023-07-24 13:07:38 +0300

    MySQL QUERIES

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,9 @@
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,7 @@
+-- Computes the average score of all records in the table second_table in my MySQL server.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;  
+#-- Computes the average score of all records in the table second_table in my MySQL server.
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;  
+#
